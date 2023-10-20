7bef2c49494316f7c8b4993b945813a5aa15ef76
kate <k_czarnota@wp.pl>
2023-10-20 15:01:17 +0200

    zadanie 47870

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let firstNumber = 7;
+
+let secondNumber = 13;
+
+function suma(arg1, arg2) {
+    let result = arg1 + arg2;
+    return result;
+  }
+
+  console.log(suma (firstNumber, secondNumber) );
